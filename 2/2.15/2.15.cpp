c64443c11aa3b99bf515f3c54107feae3742e523
jonny4547 <joaoapereira21@hotmail.com>
2021-03-23 12:35:40 +0000

    Do 2.15

--- 2/2.15/2.15.cpp
+++ 2/2.15/2.15.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 #include <random>
+#include <ctime>
 
-//TODO
 int main()
 {
+    srand(time(nullptr));
+    int a = 2 + rand() % 8, b = 2 + rand() % 8, result = a * b, input;
+
+    std::cout << a << " × " << b << " = ";
+    time_t start = time(nullptr);
+    std::cin >> input;
+    double t = difftime(time(nullptr), start);
+
+    if (result != input)
+        std::cout << "Very Bad" << std::endl;
+    else if (t > 10)
+        std::cout << "Insufficient" << std::endl;
+    else if (t > 5)
+        std::cout << "Satisfactory" << std::endl;
+    else
+        std::cout << "Good" << std::endl;
 }
